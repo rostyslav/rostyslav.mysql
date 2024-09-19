0d81684a3227e54a7ffc69c25fd2b68582d0db5e
Rostyslav Rava <rostyslav@ravasoft.com>
2024-09-19 20:18:08 +0300

    Added script for publishing collection

---
+++ publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if version parameter is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+VERSION=$1
+
+# Update the version in galaxy.yml
+sed -i "s/^version:.*/version: $VERSION/" galaxy.yml
+
+git add galaxy.yml
+git commit -m "Update the version to $VERSION"
+git push
+
+# Build the collection
+ansible-galaxy collection build
+
+# Find the built tarball
+TARBALL=$(ls -t *.tar.gz | head -n 1)
+
+ansible-galaxy collection publish "$TARBALL" -vvvv
+
+# Tag the version in git and push the tag
+git tag $VERSION
+git push --tags
+
+echo "Collection version $VERSION published to Ansible Galaxy and tagged in git."
+
+rm -f $TARBALL
+
